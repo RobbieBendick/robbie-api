7504e31f14347b13a5527d2a9323a35623b076d7
Robbie Bendick <robbiebendick@gmail.com>
2022-06-21 10:14:37 -0700

    add compose, posts, EJS, and individual clickable posts

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
+const ejs = require('ejs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+var router = express.Router();
+var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
+var composeRouter = require('./routes/compose');
+
 
 const port = 5000;
 
@@ -13,35 +18,71 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-require('dotenv').config()
+require('dotenv').config();
 
-mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1kwptpo.mongodb.net/?retryWrites=true&w=majority/blog`)
-
+mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.1kwptpo.mongodb.net/?retryWrites=true&w=majority/blog`);
 
 const postSchema = {
   title: String,
   content: String
 };
 
-
 const Post = mongoose.model("Post", postSchema);
 
-app.use('/blog', indexRouter);
+// home route
+app.get('/', (req, res, next) => {
+  Post.find({}, (err, posts) => {
+    res.render('home', {
+      posts: posts,
+    });
+  });
+});
+
+// compose route
+app.get('/compose', (req, res, next) => {
+  res.render('compose');
+});
+
+// creating new post
+app.post('/compose', (req, res) => {
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  post.save(err => {
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+// renders specific blog-post user clicks on
+app.get("/posts/:postId", (req, res) => {
+
+  const requestedPostId = req.params.postId;
+    Post.findOne({_id: requestedPostId}, (err, post) => {
+      res.render("post", {
+        title: post.title,
+        content: post.content
+      });
+    });
+  });
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -51,8 +92,6 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
-})
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = app;
